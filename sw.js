6636047b7fabb5b572c9589f633278e976664a27
kamalsinghyadav <workiflysolutionsgst@gmail.com>
2026-06-01 23:18:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'bizcrm-v1';
-const FILES = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
+const FILES = ['/index.html', '/style.css', '/script.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
